9728214012817ceb2b0f3792b18d34496f1aad4a
Nick Pozniak <87877391+imwrdo@users.noreply.github.com>
2024-03-24 17:13:44 +0100

    Add files via upload

--- Projekt_1/projekt.py
+++ Projekt_1/projekt.py
@@ -33,9 +33,10 @@ MACD = np.array(EMA_12) - np.array(EMA_26)
 SIGNAL = exponential_moving_average(MACD,9)
 
 # Initialize capital and shares
-initial_capital = 1000
-shares_owned = 1000
 
+shares_owned = 1000
+money_owned_before = shares_owned*y[1]
+money_owned = 0
 # Transaction statistics
 transaction_stats = {'Date': [], 'Type': [], 'Price': [], 'Shares': [], 'Profit/Loss': []}
 
@@ -43,45 +44,45 @@ transaction_stats = {'Date': [], 'Type': [], 'Price': [], 'Shares': [], 'Profit/
 for i in range(1, len(MACD)):
     if MACD[i] > SIGNAL[i] and MACD[i - 1] <= SIGNAL[i - 1]:
         # Buy shares
-        price = y[i - 1]  # Assuming buying at the closing price of the previous day
-        shares_bought = 0.01  # Buying 0.01 part of the share
+        price = y[i]  # Assuming buying at the closing price of the current day
+        shares_bought = round(money_owned/price,2)
         shares_owned += shares_bought
-        transaction_stats['Date'].append(data['Data'][i - 1])
-        transaction_stats['Type'].append('Buy')
+        transaction_stats['Date'].append(data['Data'][i])
+        transaction_stats['Type'].append('Kupno')
         transaction_stats['Price'].append(price)
         transaction_stats['Shares'].append(shares_bought)
         transaction_stats['Profit/Loss'].append(0)  # No profit/loss for buying
     elif MACD[i] < SIGNAL[i] and MACD[i - 1] >= SIGNAL[i - 1]:
         # Sell shares
-        price = y[i - 1]  # Assuming selling at the closing price of the previous day
-        shares_sold = shares_owned * 0.01  # Selling 0.01 part of the owned shares
+        price = y[i]  # Assuming selling at the closing price of the current day
+        shares_sold = shares_owned   # Selling all owned shares
         shares_owned -= shares_sold
-        transaction_stats['Date'].append(data['Data'][i - 1])
-        transaction_stats['Type'].append('Sell')
+        money_owned = shares_sold*price
+        transaction_stats['Date'].append(data['Data'][i])
+        transaction_stats['Type'].append('Sprzedaż')
         transaction_stats['Price'].append(price)
         transaction_stats['Shares'].append(shares_sold)
-        transaction_stats['Profit/Loss'].append((price - y[i - 2]) * shares_sold)  # Profit/Loss calculation
+        transaction_stats['Profit/Loss'].append((price - y[i - 1]) * shares_sold)  # Profit/Loss calculation
 
+money_owned_after = money_owned 
 # Calculate total profit/loss
 total_profit_loss = sum(transaction_stats['Profit/Loss'])
 
-# Draw conclusions about the usefulness of MASD
-if total_profit_loss > 0:
-    print("MACD strategy is profitable.")
-else:
-    print("MACD strategy is not profitable.")
+
 
 # Print transaction statistics
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 transaction_df = pd.DataFrame(transaction_stats)
-print("\nTransaction Statistics:")
+print("\nStatystyka transakcji:")
 print(transaction_df)
-print("\nTotal cash capital :")
-print(1000 + total_profit_loss)
-print("\nProfit/loss :")
-print(total_profit_loss)
+print("\nCałkowity kapitał pieniężny(na samym początku):")
+print(round(money_owned_before,2))
+print("\nCałkowity kapitał pieniężny(po 1000 dniach):")
+print(round(money_owned_after,2))
+print("\nZysk/strata :")
+print(round(money_owned_after-money_owned_before,3))
 
 
 
@@ -91,7 +92,7 @@ plt.figure(figsize=(10, 6))
 # Plotting Financial Instrument's Closing Prices
 plt.subplot(2, 1, 1)
 plt.plot(x, y, label='Ceny zamknięcia', color='blue')
-plt.title('Instrumentu finansowy cen zamknięcia')
+plt.title('Instrument finansowy cen zamknięcia')
 plt.xlabel('Dni')
 plt.ylabel('Cena')
 plt.legend()
@@ -99,8 +100,8 @@ plt.legend()
 
 # Plotting MACD and SIGNAL with buy/sell points
 plt.subplot(2, 1, 2)
-plt.plot(x, MACD, label='MACD', color='red')
-plt.plot(x, SIGNAL, label='SIGNAL', color='green')
+plt.plot(x, MACD, label='MACD', color='purple',zorder=1)
+plt.plot(x, SIGNAL, label='SIGNAL', color='orange',zorder=1)
 plt.title('MACD and SIGNAL')
 plt.xlabel('Dni')
 plt.ylabel('Wartość')
@@ -118,10 +119,10 @@ for i in range(1, len(MACD)):
         intersection_points.append((x[i], MACD[i]))
 
 buy_x, buy_y = zip(*intersection_points)
-plt.scatter(buy_x, buy_y, color='blue', marker='o', label='Punkty przecięcia')
 
-plt.scatter(buy_signals, MACD[buy_signals], color='green', marker='^', label='Kupno')
-plt.scatter(sell_signals, MACD[sell_signals], color='red', marker='v', label='Sprzedaż')
+
+plt.scatter(buy_signals, MACD[buy_signals], color='green', marker='^',edgecolor='black', linewidth=1.0, label='Kupno',zorder = 2)
+plt.scatter(sell_signals, MACD[sell_signals], color='red', marker='v',edgecolor='black', linewidth=1.0, label='Sprzedaż', zorder=2)
 
 plt.legend()
 plt.grid(True)
@@ -132,15 +133,15 @@ plt.show()
 plt.figure(figsize=(12, 4))
 
 # Plotting Financial Instrument's Closing Prices
-plt.plot(x, y, label='Ceny zamknięcia', color='blue')
+plt.plot(x, y, label='Ceny zamknięcia', color='blue',zorder=1)
 plt.title('Instrument finansowy cen zamknięcia wraz z punktami kupna/sprzedaży')
 plt.xlabel('Dni')
 plt.ylabel('Cena')
 plt.legend()
 
 # Plotting Buy/Sell Points on Financial Instrument's Closing Prices
-plt.scatter(buy_signals, y[buy_signals], color='green', marker='^', label='Punkt kupna')
-plt.scatter(sell_signals, y[sell_signals], color='red', marker='v', label='Punkt sprzedaży')
+plt.scatter(buy_signals, y[buy_signals], color='green', marker='^', edgecolor='black', linewidth=1.0, label='Punkt kupna',zorder=2)
+plt.scatter(sell_signals, y[sell_signals], color='red', marker='v', edgecolor='black', linewidth=1.0, label='Punkt sprzedaży',zorder=2)
 
 plt.legend()
 plt.grid(True)
